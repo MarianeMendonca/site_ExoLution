f4dae54b26f4b548b01676e767e7294a16af6254
Mariane Mendonca <mariane.mendonca098@gmail.com>
2025-09-23 19:19:07 -0300

    add ApreProd

---
+++ section/secApreProd.php
@@ -0,0 +1,27 @@
+<section>
+    <div class="product-container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="../imagem/brinquedos.png" alt="Nome do Produto" class="product-image">
+            </div>
+            <div class="col-md-6">
+                <div class="product-details">
+                    <h1>Nome do Produto</h1>
+                    <p class="description">
+                        Descrição detalhada do produto. Aqui você pode colocar informações sobre o animal exótico, cuidados especiais, tamanho, idade recomendada, alimentação e qualquer detalhe que ajude o tutor a tomar a decisão.
+                    </p>
+                    <div class="product-info">
+                        <div><span>Preço:</span> R$ 250,00</div>
+                        <div><span>Categoria:</span> Répteis, Brinquedos</div>
+                        <div><span>Disponibilidade:</span> Em estoque</div>
+                        <div><span>Cuidados:</span> Necessita de terrário aquecido, alimentação diária com insetos vivos.</div>
+                    </div>
+                    <div class="button">
+                        <button class="btn carrinho">Adicionar ao Carrinho</button>
+                        <button class="btn buy">Comprar Agora</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
