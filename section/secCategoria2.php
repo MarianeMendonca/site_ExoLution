d63debce8b9cfa07f75079d642a6d25468ad478e
Mariane Mendonca <mariane.mendonca098@gmail.com>
2025-11-24 21:00:10 -0300

    update categorias

--- section/secCategoria2.php
+++ section/secCategoria2.php
@@ -12,26 +12,96 @@
             <div class="cardGeral">
                 <div class="card">
                     <a href="#">
-                        <img src="../imagem/anfibio.png" alt="">
+                        <img src="../imagem/aquario.png" alt="Um aquário para répteis">
                     </a>
                     <div class="inf">
                         <h2>Aquários / Terrários</h2>
                     </div>
                 </div>
                 <div class="card">
-
+                    <a href="#">
+                        <img src="../imagem/brinquedos.png" alt="Vários brinquedos de animais">
+                    </a>
+                    <div class="inf">
+                        <h2>Brinquedos</h2>
+                    </div>
                 </div>
                 <div class="card">
-
+                    <a href="#">
+                        <img src="../imagem/higiene.png" alt="Vários produtos de higiene para animais">
+                    </a>
+                    <div class="inf">
+                        <h2>Higiene</h2>
+                    </div>
                 </div>
                 <div class="card">
-
+                    <a href="#">
+                        <img src="../imagem/racao.png" alt="Vários grãos de rações">
+                    </a>
+                    <div class="inf">
+                        <h2>Rações</h2>
+                    </div>
                 </div>
                 <div class="card">
-
+                    <a href="#">
+                        <img src="../imagem/roupas.png" alt="Várias roupas para animais">
+                    </a>
+                    <div class="inf">
+                        <h2>Roupas</h2>
+                    </div>
                 </div>
                 <div class="card">
-
+                    <a href="#">
+                        <img src="../imagem/outros.png" alt="Outros produtos para animais">
+                    </a>
+                    <div class="inf">
+                        <h2>Outros</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="produtos">
+            <h2 class="sub" style="margin-top: 70px;">Animais</h2>
+            <div class="cardGeral">
+                <div class="card">
+                    <a href="#">
+                        <img src="../imagem/aves.png" alt="Um aquário para répteis">
+                    </a>
+                    <div class="inf">
+                        <h2>Aves</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="#">
+                        <img src="../imagem/anfibio.png" alt="Vários brinquedos de animais">
+                    </a>
+                    <div class="inf">
+                        <h2>Anfíbios</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="#">
+                        <img src="../imagem/pelos.png" alt="Vários produtos de higiene para animais">
+                    </a>
+                    <div class="inf">
+                        <h2>Mamíferos</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="#">
+                        <img src="../imagem/peixes.png" alt="Vários grãos de rações">
+                    </a>
+                    <div class="inf">
+                        <h2>Peixes</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="#">
+                        <img src="../imagem/escamas.png" alt="Várias roupas para animais">
+                    </a>
+                    <div class="inf">
+                        <h2>Répteis</h2>
+                    </div>
                 </div>
             </div>
         </div>
